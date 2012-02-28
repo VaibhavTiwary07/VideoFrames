2cf0d6e3c5c333bd0541f9dd8ead7f9ccc85b92a
Sunitha Gadigota <sunitha@Sunithas-MacBook-Air.local>
2012-02-27 21:05:18 -0500

    Initial Commit

---
+++ PicFrames/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  PicFrames
+//
+//  Created by Sunitha Gadigota on 2/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
---
+++ PicFramesTests/PicFramesTests.m
@@ -0,0 +1,32 @@
+//
+//  PicFramesTests.m
+//  PicFramesTests
+//
+//  Created by Sunitha Gadigota on 2/27/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "PicFramesTests.h"
+
+@implementation PicFramesTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in PicFramesTests");
+}
+
+@end
